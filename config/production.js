bf5dd6e6430eff74631e268a6bb32a645377ef30
Jared Palmer <jaredpalmer>
2016-06-27 16:15:25 -0400

    modify for heroku deployment

---
+++ config/production.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'mailtrain',
+  log: {
+    level: 'verbose'
+  },
+  www: {
+    port: process.env.PORT || 5000,
+    host: '0.0.0.0',
+    secret: process.env.SESSION_SECRET || 'a cat',
+    remember: process.env.SESSION_LENGTH || 2592000,
+    log: 'dev',
+    proxy: false,
+    postsize: process.env.POSTSIZE || '2MB'
+  },
+  mysql: `${process.env.JAWSDB_MARIA_URL}?charset=utf8mb4`,
+  redis: {
+    enabled: false
+  },
+  verp: {
+    enabled: false
+  }
+}
